c218a01ab6fdba745bd66aea332257964e884a91
Fernando Alvarez <info@fer.ac>
2017-05-13 19:56:34 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,31 @@
+httpfs
+======
+
+httpfs is a HTTP server that translate requests into basic UNIX files and directories
+operations.
+
+HTTP->Unix Mappings
+-------------------
+
+| HTTP Request                   | UNIX operation                   |
+| ------------------------------ | -------------------------------- |
+|  GET /$FILE_PATH               | `cat $HOME/$FILE_PATH`           |
+|  GET /$DIR_PATH                | `ls $HOME/DIR_PATH`              |
+|  POST /$SOME_PATH type=file    | `touch $HOME/SOME_PATH`          |
+|  POST /$SOME_PATH type=dir     | `mkdir -p $HOME/SOME_PATH`       |
+|  PUT /$FILE_PATH content=text  | `echo "text" >> $HOME/FILE_PATH` |
+|  DELETE /$FILE_PATH            | `rm $HOME/$FILE_PATH`            |
+|  DELETE /$DIR_PATH             | `rm -rf $HOME/DIR_PATH`          |
+
+Install
+-------
+
+```
+go get -u github.com/oscillatingworks/httpfs
+httpfs
+```
+
+LICENSE
+-------
+
+MIT
---
+++ main.go
@@ -0,0 +1,206 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"runtime"
+)
+
+// UserHomeDir returns the path of your HOME directory
+func UserHomeDir() string {
+	env := "HOME"
+	if runtime.GOOS == "windows" {
+		env = "USERPROFILE"
+	} else if runtime.GOOS == "plan9" {
+		env = "home"
+	}
+	return os.Getenv(env)
+}
+
+// IsFile tells you whether a file is a file,
+// a directory, or does not exist.
+// true, nil -> file
+// false, nil -> dir
+// _, err -> not exist
+func IsFile(path string) (bool, error) {
+	fi, err := os.Stat(path)
+	if err != nil {
+		return false, err
+	}
+
+	switch mode := fi.Mode(); {
+	case mode.IsDir():
+		return false, nil
+	case mode.IsRegular():
+		return true, nil
+	}
+
+	return false, errors.New("will not happen")
+}
+
+// HTTPFS represents a httpfs handler
+type HTTPFSHandler struct {
+	path   string
+	err    error
+	isFile bool
+}
+
+// CatFile reproduces `cat $file_path` unix operation
+func (h *HTTPFSHandler) CatFile(w http.ResponseWriter, r *http.Request) {
+	b, err := ioutil.ReadFile(h.path)
+	if err != nil {
+		h.err = err
+		h.HandleNotFound(w, r)
+		return
+	}
+
+	log.Printf("path=%s,method=%s,unix_op=cat %s,response_code=%d", h.path, r.Method, h.path, http.StatusOK)
+	_, _ = w.Write(b)
+}
+
+// LsDir reproduces `ls $dir_path` unix operation
+func (h *HTTPFSHandler) LsDir(w http.ResponseWriter, r *http.Request) {
+	files, err := ioutil.ReadDir(h.path)
+	if err != nil {
+		h.err = err
+		h.HandleNotFound(w, r)
+		return
+	}
+
+	for _, file := range files {
+		line := fmt.Sprintf("%s\n", file.Name())
+		w.Write([]byte(line))
+	}
+}
+
+// TouchFile reproduces `touch $file_path` unix operation
+func (h *HTTPFSHandler) TouchFile(w http.ResponseWriter, r *http.Request) {
+	_, err := os.OpenFile(h.path, os.O_RDONLY|os.O_CREATE, 0644)
+	if err != nil {
+		h.err = err
+		h.HandleConflict(w, r)
+		return
+	}
+
+	log.Printf("path=%s,method=%s,unix_op=touch %s,response_code=%d", h.path, r.Method, h.path, http.StatusCreated)
+	w.WriteHeader(http.StatusCreated)
+}
+
+// HandleNotFound handles NotFound scenarios
+func (h *HTTPFSHandler) HandleNotFound(w http.ResponseWriter, r *http.Request) {
+	log.Printf("path=%s,method=%s,response_code=%d,err=%s", h.path, r.Method, http.StatusNotFound, h.err)
+	http.NotFound(w, r)
+}
+
+// HandleConflict handles Conflict scenarios
+func (h *HTTPFSHandler) HandleConflict(w http.ResponseWriter, r *http.Request) {
+	log.Printf("path=%s,method=%s,response_code=%d,err=%s", h.path, r.Method, http.StatusConflict, h.err)
+	w.WriteHeader(http.StatusConflict)
+	io.WriteString(w, "conflict\n")
+}
+
+// HandleBadRequest handles BadRequest scenarios
+func (h *HTTPFSHandler) HandleBadRequest(w http.ResponseWriter, r *http.Request) {
+	log.Printf("path=%s,method=%s,response_code=%d,err=%s", h.path, r.Method, http.StatusBadRequest, h.err)
+	w.WriteHeader(http.StatusConflict)
+	io.WriteString(w, "bad request\n")
+}
+
+// HandleGet handles GET requests
+func (h *HTTPFSHandler) HandleGet(w http.ResponseWriter, r *http.Request) {
+	if h.isFile {
+		h.CatFile(w, r)
+		return
+	}
+
+	h.LsDir(w, r)
+}
+
+// HandlePost handles POST requests
+func (h *HTTPFSHandler) HandlePost(w http.ResponseWriter, r *http.Request) {
+	// if file exists we exit
+	if h.err == nil {
+		h.err = errors.New("file/dir exists")
+		h.HandleConflict(w, r)
+		return
+
+	}
+
+	// parse form looking for type
+	r.ParseForm()
+	t := r.PostFormValue("type")
+	if t == "" {
+		h.err = errors.New("type missing")
+		h.HandleBadRequest(w, r)
+		return
+	}
+
+	if t == "file" {
+		h.TouchFile(w, r)
+		return
+	}
+
+	if t == "dir" {
+		// TODO
+		log.Printf("path=%s,method=%s,response_code=%d,err=%s", h.path, r.Method, http.StatusNotImplemented, "not implemented")
+		w.WriteHeader(http.StatusNotImplemented)
+		return
+	}
+
+	h.err = errors.New("wrong type")
+	h.HandleBadRequest(w, r)
+}
+
+// HandlePut handles PUT requests
+func (h *HTTPFSHandler) HandlePut(w http.ResponseWriter, r *http.Request) {
+	// TODO
+}
+
+// HandleDelete handles DELETE requests
+func (h *HTTPFSHandler) HandleDelete(w http.ResponseWriter, r *http.Request) {
+	// TODO
+}
+
+// ServeHTTP satisfies http.Handler interface
+func (h *HTTPFSHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	// build file/dir path
+	h.path = fmt.Sprintf("%s%s", UserHomeDir(), r.URL)
+	h.err = nil
+
+	// find if path is file or directory
+	isFile, err := IsFile(h.path)
+	if err != nil {
+		h.err = err
+	}
+	h.isFile = isFile
+
+	switch {
+	case r.Method == "GET":
+		// cat and ls
+		h.HandleGet(w, r)
+		return
+	case r.Method == "POST":
+		// touch and mkdir
+		h.HandlePost(w, r)
+		return
+	case r.Method == "PUT":
+		// echo
+		h.HandlePut(w, r)
+		return
+	case r.Method == "DELETE":
+		// rm
+		h.HandleDelete(w, r)
+		return
+	}
+}
+
+func main() {
+	http.Handle("/", &HTTPFSHandler{})
+	log.Println("Listening on :8080...")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
