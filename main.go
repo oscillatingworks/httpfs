b66ca4400580adaacf75f3f52c95ac5df45bf478
Fernando Alvarez <info@fer.ac>
2017-05-19 01:39:20 +0200

    Resort functions

--- main.go
+++ main.go
@@ -10,11 +10,121 @@ import (
 	"os"
 )
 
+func main() {
+	// TODO custom port
+	http.Handle("/", &HTTPFSHandler{})
+	log.Println("Listening on :8080...")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
 // HTTPFS represents a httpfs handler
 type HTTPFSHandler struct {
 	path   string
 	err    error
 	isFile bool
+}
+
+// ServeHTTP satisfies http.Handler interface
+func (h *HTTPFSHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	// build file/dir path
+	h.path = fmt.Sprintf("%s%s", UserHomeDir(), r.URL)
+	h.err = nil
+
+	// find if path is file or directory
+	isFile, err := IsFile(h.path)
+	if err != nil {
+		h.err = err
+	}
+	h.isFile = isFile
+
+	switch {
+	case r.Method == "GET":
+		// cat and ls
+		h.HandleGet(w, r)
+		return
+	case r.Method == "POST":
+		// touch and mkdir
+		h.HandlePost(w, r)
+		return
+	case r.Method == "PUT":
+		// echo
+		h.HandlePut(w, r)
+		return
+	case r.Method == "DELETE":
+		// rm
+		// TODO
+		return
+	}
+}
+
+// Handlers
+
+// HandleGet handles GET requests
+func (h *HTTPFSHandler) HandleGet(w http.ResponseWriter, r *http.Request) {
+	if h.isFile {
+		h.CatFile(w, r)
+		return
+	}
+
+	h.LsDir(w, r)
+}
+
+// HandlePost handles POST requests
+func (h *HTTPFSHandler) HandlePost(w http.ResponseWriter, r *http.Request) {
+	// if file exists we exit
+	if h.err == nil {
+		h.err = errors.New("file/dir exists")
+		h.HandleConflict(w, r)
+		return
+
+	}
+
+	// parse form looking for type
+	r.ParseForm()
+	t := r.PostFormValue("type")
+	if t == "" {
+		h.err = errors.New("type missing")
+		h.HandleBadRequest(w, r)
+		return
+	}
+
+	if t == "file" {
+		h.TouchFile(w, r)
+		return
+	}
+
+	if t == "dir" {
+		// TODO
+		return
+	}
+
+	h.err = errors.New("wrong type")
+	h.HandleBadRequest(w, r)
+}
+
+// HandlePut handles PUT requests
+func (h *HTTPFSHandler) HandlePut(w http.ResponseWriter, r *http.Request) {
+	// TODO
+}
+
+// HandleNotFound handles NotFound scenarios
+func (h *HTTPFSHandler) HandleNotFound(w http.ResponseWriter, r *http.Request) {
+	log.Printf("path=%s,method=%s,response_code=%d,err=%s", h.path, r.Method, http.StatusNotFound, h.err)
+	http.NotFound(w, r)
+}
+
+// HandleConflict handles Conflict scenarios
+func (h *HTTPFSHandler) HandleConflict(w http.ResponseWriter, r *http.Request) {
+	log.Printf("path=%s,method=%s,response_code=%d,err=%s", h.path, r.Method, http.StatusConflict, h.err)
+	w.WriteHeader(http.StatusConflict)
+	io.WriteString(w, "conflict\n")
+}
+
+// HandleBadRequest handles BadRequest scenarios
+func (h *HTTPFSHandler) HandleBadRequest(w http.ResponseWriter, r *http.Request) {
+	log.Printf("path=%s,method=%s,response_code=%d,err=%s", h.path, r.Method, http.StatusBadRequest, h.err)
+	w.WriteHeader(http.StatusConflict)
+	io.WriteString(w, "bad request\n")
 }
 
 // UNIX calls
@@ -58,120 +168,4 @@ func (h *HTTPFSHandler) TouchFile(w http.ResponseWriter, r *http.Request) {
 
 	log.Printf("path=%s,method=%s,unix_op=touch %s,response_code=%d", h.path, r.Method, h.path, http.StatusCreated)
 	w.WriteHeader(http.StatusCreated)
-}
-
-// Handlers
-
-// HandleNotFound handles NotFound scenarios
-func (h *HTTPFSHandler) HandleNotFound(w http.ResponseWriter, r *http.Request) {
-	log.Printf("path=%s,method=%s,response_code=%d,err=%s", h.path, r.Method, http.StatusNotFound, h.err)
-	http.NotFound(w, r)
-}
-
-// HandleConflict handles Conflict scenarios
-func (h *HTTPFSHandler) HandleConflict(w http.ResponseWriter, r *http.Request) {
-	log.Printf("path=%s,method=%s,response_code=%d,err=%s", h.path, r.Method, http.StatusConflict, h.err)
-	w.WriteHeader(http.StatusConflict)
-	io.WriteString(w, "conflict\n")
-}
-
-// HandleBadRequest handles BadRequest scenarios
-func (h *HTTPFSHandler) HandleBadRequest(w http.ResponseWriter, r *http.Request) {
-	log.Printf("path=%s,method=%s,response_code=%d,err=%s", h.path, r.Method, http.StatusBadRequest, h.err)
-	w.WriteHeader(http.StatusConflict)
-	io.WriteString(w, "bad request\n")
-}
-
-// HandleGet handles GET requests
-func (h *HTTPFSHandler) HandleGet(w http.ResponseWriter, r *http.Request) {
-	if h.isFile {
-		h.CatFile(w, r)
-		return
-	}
-
-	h.LsDir(w, r)
-}
-
-// HandlePost handles POST requests
-func (h *HTTPFSHandler) HandlePost(w http.ResponseWriter, r *http.Request) {
-	// if file exists we exit
-	if h.err == nil {
-		h.err = errors.New("file/dir exists")
-		h.HandleConflict(w, r)
-		return
-
-	}
-
-	// parse form looking for type
-	r.ParseForm()
-	t := r.PostFormValue("type")
-	if t == "" {
-		h.err = errors.New("type missing")
-		h.HandleBadRequest(w, r)
-		return
-	}
-
-	if t == "file" {
-		h.TouchFile(w, r)
-		return
-	}
-
-	if t == "dir" {
-		// TODO
-		log.Printf("path=%s,method=%s,response_code=%d,err=%s", h.path, r.Method, http.StatusNotImplemented, "not implemented")
-		w.WriteHeader(http.StatusNotImplemented)
-		return
-	}
-
-	h.err = errors.New("wrong type")
-	h.HandleBadRequest(w, r)
-}
-
-// HandlePut handles PUT requests
-func (h *HTTPFSHandler) HandlePut(w http.ResponseWriter, r *http.Request) {
-	// TODO
-}
-
-// HandleDelete handles DELETE requests
-func (h *HTTPFSHandler) HandleDelete(w http.ResponseWriter, r *http.Request) {
-	// TODO
-}
-
-// ServeHTTP satisfies http.Handler interface
-func (h *HTTPFSHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	// build file/dir path
-	h.path = fmt.Sprintf("%s%s", UserHomeDir(), r.URL)
-	h.err = nil
-
-	// find if path is file or directory
-	isFile, err := IsFile(h.path)
-	if err != nil {
-		h.err = err
-	}
-	h.isFile = isFile
-
-	switch {
-	case r.Method == "GET":
-		// cat and ls
-		h.HandleGet(w, r)
-		return
-	case r.Method == "POST":
-		// touch and mkdir
-		h.HandlePost(w, r)
-		return
-	case r.Method == "PUT":
-		// echo
-		h.HandlePut(w, r)
-		return
-	case r.Method == "DELETE":
-		// rm
-		h.HandleDelete(w, r)
-		return
-	}
-}
-
-func main() {
-	http.Handle("/", &HTTPFSHandler{})
-	log.Println("Listening on :8080...")
-	log.Fatal(http.ListenAndServe(":8080", nil))
 }
